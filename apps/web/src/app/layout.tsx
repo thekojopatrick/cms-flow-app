4a0345b7c5c386bea84e0e39967899aca0194244
thekojopatrick <patrickasare84@gmail.com>
2025-08-06 19:21:53 +0000

    fix overflow issue

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -30,10 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <div className="grid grid-rows-[auto_1fr] h-svh">
-            {/* <Header /> */}
             {children}
-          </div>
+        
         </Providers>
       </body>
     </html>
